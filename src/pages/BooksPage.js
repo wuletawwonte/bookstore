bb013a80c9b09e7f79fa5681f6b69043ee967c17
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-30 10:24:11 +0300

    Add Book and BooksList components

--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import BooksList from '../components/BooksList';
 
 const BooksPage = () => {
   return (
+    <>
     <h2>Books page</h2>
+    <BooksList />
+    </>
   );
 }
 
