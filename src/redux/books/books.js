e9be0f5e893bd1da0d5e84a2175f72bbbdf3db68
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-03 09:47:25 +0300

    Add book actions creators

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,3 +1,7 @@
+// Action types
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
+// Action creators
+const addBook = (title) => ({ type: ADD_BOOK, payload: { title },});
+const removeBook = (id) => ({ type: REMOVE_BOOK, payload: { id },});
