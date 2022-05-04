78fd3f62138b0516a8ebe10beadcbda1dcde7949
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-04 09:56:09 +0300

    Add form submit handler for book registration

--- src/components/RegisterBook.js
+++ src/components/RegisterBook.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-const RegisterBook = () => (
-  <>
-    <h2>Register Book</h2>
-    <form className="register-book-form">
-      <input type="text" name="title" placeholder="Title" />
-      <input type="text" name="author" placeholder="Author" />
-      <button type="submit">Register</button>
-    </form>
-  </>
-);
+const RegisterBook = () => {
+  const registerBook = (e) => {
+    e.preventDefault();
+    console.log('form submit clicked');
+  };
+
+  return (
+    <>
+      <h2>Register Book</h2>
+      <form className="register-book-form" onSubmit={registerBook}>
+        <input type="text" name="title" placeholder="Title" required />
+        <input type="text" name="author" placeholder="Author" required />
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
 
 export default RegisterBook;
