096518521342b6993048d87004f7a637ac6d56cc
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-04 09:51:38 +0300

    Load initial data from redux store

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import Book from './Book';
 import { useSelector } from 'react-redux';
+import Book from './Book';
 
 const BookList = () => {
-  const books = [
-    {
-      id: 1,
-      name: 'The Pursuits of Happiness',
-      category: 'Politics',
-      author: 'Anna Frank',
-    },
-    {
-      id: 2,
-      name: 'The Lord of the Rings',
-      category: 'Biography',
-      author: 'Ben Hur',
-    },
-    {
-      id: 3,
-      name: 'Alice in Wonderland',
-      category: 'History',
-      author: 'CS Lewis',
-    },
-  ];
+  const books = useSelector((state) => state.books);
+
   return (
     <div>
       <ul className="books-list">
--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
 );
